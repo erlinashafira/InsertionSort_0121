f05438699725014c851531b9c31ad104ab4fe76b
errerew <erlina31acc@gmail.com>
2025-03-25 15:25:51 +0700

    bagian 2

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -15,4 +15,12 @@ if (n <= 20)
 { // Membuat Kondisi n tidak lebih dari 20
     break;
 }
+else
+{
+    cout << "\nArray yang anda masukkan maksimal 2 Elemen,\n"; // Menampilkan Pesan jika data lebih dari 20
 }
+}
+cout << endl; // Membuat jarak per baris program 
+cout << "======================" << endl;
+cout << "Masukkan Element Array" << endl;
+cout << "======================" << endl;
